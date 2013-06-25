4c02a8b68bc8826b012c6a6754316f17eb2628b6
brantje <brantje@gmail.com>
2013-06-25 12:17:57 +0000

    Added bounce effect

--- default.py
+++ default.py
@@ -16,6 +16,7 @@ class Screensaver(xbmcgui.WindowXMLDialog):
 
         def __init__(self, exit_callback):
             self.exit_callback = exit_callback
+
 			
         def onScreensaverDeactivated(self):
             print '3 ExitMonitor: sending exit_callback'
@@ -27,26 +28,46 @@ class Screensaver(xbmcgui.WindowXMLDialog):
 	if Addon.getSetting('hidedate') == 'true':
 		self.getControl(30106).setVisible(False)
 	print 'Setting: %s' % Addon.getSetting('movement')
-	if Addon.getSetting('movement') == '1':
-		#Random movements
-		while(True):
-			#update time
-			now  = datetime.now()
-			hour = now.hour
-			minute = now.minute
-			self.getControl(30005).setImage('clock/%s.png'%minute)
-			self.getControl(30003).setImage('clock/%s.png'%hour)
-
-			container = self.getControl(30002)
-			screenx = self.getWidth()/2-175
+	
+	self.hour_control = self.getControl(30003)
+	self.minute_control = self.getControl(30005)	
+	self.container = self.getControl(30002)
+	#bounce settings
+	# The velocity, or distance moved per time step
+	vx = 10    # x velocity
+	vy = 5    # y velocity
+	while(True):
+		#update time
+		now  = datetime.now()
+		hour = now.hour
+		minute = now.minute
+		self.minute_control.setImage('clock/%s.png'%minute)
+		self.hour_control.setImage('clock/%s.png'%hour)
+		#Random movements  movement = 1
+		if Addon.getSetting('movement') == '1':
+			screenx = self.getWidth()/2-185
 			screeny = self.getHeight()/2-75
 			print 'Screen X : %s' % screenx	
 			print 'Screen Y : %s' % screeny	
 			new_x = random.randint(-screenx,screenx)
 			new_y = random.randint(-screeny,screeny)
-			container.setPosition(new_x,new_y)
-
+			self.container.setPosition(new_x,new_y)
 			xbmc.sleep(3000)
+		#Bounce movement=2
+		if Addon.getSetting('movement') == '2':
+			self.currentposition = self.container.getPosition()
+			screenx = self.getWidth()/2-200
+			screeny = self.getHeight()/2-75
+			
+			new_x = self.currentposition[0]+vx
+			new_y = self.currentposition[1]+vy
+			if new_x >= screenx or new_x <= -screenx:
+				vx = vx*-1
+			if new_y >= screeny or new_y <= -screeny:
+				vy = vy*-1
+			self.container.setPosition(new_x,new_y)
+			xbmc.sleep(100)
+		
 	
     def exit(self):
         print '4 Screensaver: Exit requested'
